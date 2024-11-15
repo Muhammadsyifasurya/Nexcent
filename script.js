be8c47ddf513afaf934a4948f88ca108eb4f8f85
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2024-11-15 15:41:42 +0700

    update html, css, js include feature dark mode

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function toggleMenu(element) {
   console.log("Hamburger clicked");
   console.log("Nav list is visible:", isVisible);
 }
+
+const toggleButton = document.getElementById("toggleDarkMode");
+const body = document.body;
+
+// Cek apakah dark mode diaktifkan sebelumnya
+if (localStorage.getItem("dark-mode") === "enabled") {
+  body.classList.add("dark-mode");
+  toggleButton.classList.add("dark-mode");
+}
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  toggleButton.classList.toggle("dark-mode");
+
+  // Simpan preferensi pengguna
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "enabled");
+  } else {
+    localStorage.setItem("dark-mode", "disabled");
+  }
+});
